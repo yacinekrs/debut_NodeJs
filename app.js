710bd6fa7647689da591a9904bd73a2bf47319f7
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-08-28 01:56:08 +0200

    envoie de reponse serveur

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, rep) => {
-  console.log("c est fait ta fait une requete http");
+  rep.setHeader("Cotent-Type", "text/html"); // le type du contenu de la reponse est html
+  rep.write("<html>");
+  rep.write("<head><title> ma premiere page </title><head>");
+  rep.write("<body><h1> Helle world de serveur node.js <h1></body>");
+  rep.write("<body><h2> Helle world de serveur node.js <h2></body>");
+  rep.write("</html>");
+  rep.end;
 });
 
 server.listen(3000);
