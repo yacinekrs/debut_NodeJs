73ce99df19f76901c92bb8b35cf5d745bb142e3e
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-08-28 02:40:27 +0200

    acheminemen

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, rep) => {
+  const url = req.url;
+  if (url === "/") {
+    rep.write("<html>");
+    rep.write("<head><title>message</title><head>");
+    rep.write(
+      "<body><form action='/messge' methode><input type = 'text'> <button type='submit'>envoyer</button></form></body>"
+    );
+    rep.write("</html>");
+    return rep.end();
+  }
   rep.setHeader("Cotent-Type", "text/html"); // le type du contenu de la reponse est html
   rep.write("<html>");
-  rep.write("<head><title> ma premiere page </title><head>");
+  rep.write("<head><title> reponse serveur </title><head>");
   rep.write("<body><h1> Helle world de serveur node.js <h1></body>");
   rep.write("<body><h2> Helle world de serveur node.js <h2></body>");
   rep.write("</html>");
-  rep.end;
+  rep.end();
 });
 
 server.listen(3000);
