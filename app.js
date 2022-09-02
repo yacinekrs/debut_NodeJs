7e08795492ed79e0a5017d96b4f448355ddbed99
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-09-02 16:47:39 +0200

    iddelware

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("le premier middelwar");
+  next(); //permet de continuer vers le prochain middelwar
+});
+
+app.use((req, res, next) => {
+  console.log("le deuxieme middelwar");
+});
+const server = http.createServer(app);
 
 server.listen(3000);
