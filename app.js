d51186818751f80c64c8390a544df697b1ba939f
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-09-07 00:53:50 +0200

    analyse de donnee

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("le premier middelware");
-  next(); //permet de continuer vers le prochain middelwar
-});
+// app.use("/", (req, res, next) => {
+//   console.log("le premier middelware");
+//   next(); //permet de continuer vers le prochain middelwar
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/production", (req, res, next) => {
+app.use("/ajout-produit", (req, res, next) => {
   //use nous permet d'ajouter une nouvelle fonction middelware
   console.log(" le deuxieme middelware");
-  res.send("<h1> page de production <h1>");
+  res.send(
+    "<form action='/produit' method='POST'><input type='text' name='title'><button type='submit'> ajouter </button></form>"
+  );
+});
+app.use("/produit", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
