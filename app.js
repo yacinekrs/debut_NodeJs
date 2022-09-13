7f0c6cbae0654692f196b220ef7b5bbdfd9bf230
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-09-13 13:12:04 +0200

    filtre path

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const res = require("express/lib/response");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminrouter);
-app.use(boutiquerouter);
+app.use("/admin", adminrouter);
+app.use(boutiquerouter); // "/admin" c'est pour dire que uniquement les path qui commence avec admin qui peuvent y'acceder
 
 app.use((req, res, next) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("Page not found"); // pour gerer les pages introuvable
+  //404 c est le code d'etat des pages introuvable
 });
 
 app.listen(3000); // il configure le serveur et le mets a l ecoute au meme temps
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,18 @@
 const express = require("express");
-
 const router = express.Router(); // creer un nouvel objet routeur pour gerer les requete
 
+// elle est atteinte que par /admin/ajout-produit => get
 router.get("/ajout-produit", (req, res, next) => {
   //use nous permet d'ajouter une nouvelle fonction middelware
   console.log(" le deuxieme middelware");
   res.send(
-    "<form action='/produit' method='POST'><input type='text' name='title'><button type='submit'> ajouter </button></form>"
+    "<form action='/admin/ajout-produit' method='POST'><input type='text' name='title'><button type='submit'> ajouter </button></form>"
   );
 });
-router.post("/produit", (req, res, next) => {
+
+// elle est atteinte que par /admin/ajout-produit => post
+
+router.post("/ajout-produit", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
