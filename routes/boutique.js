9ef5dadadef1e8bd6dbe53d266ba44c959dee276
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-09-14 15:18:40 +0200

    servir page html

--- routes/boutique.js
+++ routes/boutique.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require('path')
 
 const router = express.Router(); // creer un nouvel objet routeur pour gerer les requete
 
 router.get("/", (req, res, next) => {
   //use nous permet d'ajouter une nouvelle fonction middelware
   console.log("le troisieme middelware");
-  res.send("<h1> hello from express<h1>");
+  res.sendFile(path.join(__dirname, '../', 'views', 'boutique.html'));
 });
 
 module.exports = router;
